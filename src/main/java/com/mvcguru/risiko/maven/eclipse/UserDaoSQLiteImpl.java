727e7475606409b451bc02bc381c1e4e9edc136b
Daniele Giorgio Michele Romano <d.romano32@campus.unimib.it>
2024-02-08 11:37:03 +0100

    Commit refactoring
    Modifiche per problemi Intentionality: Use try-with-resources or close
    this "PreparedStatement" in a "finally" clause.

--- src/main/java/com/mvcguru/risiko/maven/eclipse/UserDaoSQLiteImpl.java
+++ src/main/java/com/mvcguru/risiko/maven/eclipse/UserDaoSQLiteImpl.java
@@ -21,8 +21,9 @@ public class UserDaoSQLiteImpl implements UserDao {
     @Override
     public User getUserByUsernameAndPassword(String username, String password) {
         String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+        PreparedStatement pstmt = null;
         try {
-            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt = connection.prepareStatement(sql);
             pstmt.setString(1, username);
             pstmt.setString(2, password);
             ResultSet rs = pstmt.executeQuery();
@@ -33,60 +34,107 @@ public class UserDaoSQLiteImpl implements UserDao {
             }
         } catch (SQLException e) {
             throw new RuntimeException("Errore durante il recupero dell'utente.", e);
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
+
 
     @Override
     public void registerUser(User user) {
         String sql = "INSERT INTO users(username, password) VALUES(?, ?)";
+        PreparedStatement pstmt = null;
         try {
-            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt = connection.prepareStatement(sql);
             pstmt.setString(1, user.getUsername());
             pstmt.setString(2, user.getPassword());
             pstmt.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException("Errore durante la registrazione dell'utente.", e);
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
+    
     @Override
     public void updateUser(User user) {
         String sql = "UPDATE users SET password = ? WHERE username = ?";
+        PreparedStatement pstmt = null;
         try {
-            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt = connection.prepareStatement(sql);
             pstmt.setString(1, user.getPassword());
             pstmt.setString(2, user.getUsername());
             pstmt.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException("Errore durante l'aggiornamento dell'utente.", e);
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     @Override
     public void deleteUser(User user) {
         String sql = "DELETE FROM users WHERE username = ?";
+        PreparedStatement pstmt = null;
         try {
-            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt = connection.prepareStatement(sql);
             pstmt.setString(1, user.getUsername());
             pstmt.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException("Errore durante l'eliminazione dell'utente.", e);
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
+
     
     public void createUsersTable() {
         String sql = "CREATE TABLE IF NOT EXISTS users (\n"
                 + "	username text PRIMARY KEY,\n"
                 + "	password text NOT NULL\n"
                 + ");";
-
+        PreparedStatement pstmt = null;
         try {
-            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt = connection.prepareStatement(sql);
             pstmt.execute();
         } catch (SQLException e) {
             throw new RuntimeException("Errore durante la creazione della tabella users.", e);
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
+
 
 
 }
