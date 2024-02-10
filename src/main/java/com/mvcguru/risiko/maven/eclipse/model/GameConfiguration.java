e8d33e21469cd60f790e12c75be6f48353f62633
Andrea Malnati <a.malnati3@campus.unimib.it>
2024-02-10 01:19:19 +0100

    Aggiunta libreria jackson

--- src/main/java/com/mvcguru/risiko/maven/eclipse/model/GameConfiguration.java
+++ src/main/java/com/mvcguru/risiko/maven/eclipse/model/GameConfiguration.java
@@ -1,33 +1,70 @@
 package com.mvcguru.risiko.maven.eclipse.model;
 
-import lombok.Data;
-import lombok.Getter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.io.Serializable;
+import java.util.Objects;
 
-@Data
-public class GameConfiguration {
-	
-	private enum GameMode {
-	    EASY,
-	    MEDIUM,
-	    Hard
-	}
-	
-	@Getter private GameMode mode;
-	
-	@Getter private int numberOfPlayers;
-	
-	@Getter private String idMap;
-	
-	public int getNumberOfPlayers() {
-		return numberOfPlayers;
+public class GameConfiguration implements Serializable {
+
+    @JsonProperty("difficolta")
+    private GameMode mode;
+
+    @JsonProperty("players")
+    private int numberOfPlayers;
+
+    @JsonProperty("nomeMappa")
+    private String idMap;
+
+    public void setMode(GameMode mode) {
+		this.mode = mode;
 	}
 
-	public String getMap() {
-		return idMap;
+	public void setNumberOfPlayers(int numberOfPlayers) {
+		this.numberOfPlayers = numberOfPlayers;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idMap, mode, numberOfPlayers);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameConfiguration other = (GameConfiguration) obj;
+		return Objects.equals(idMap, other.idMap) && mode == other.mode && numberOfPlayers == other.numberOfPlayers;
+	}
+
+	@Override
+	public String toString() {
+		return "GameConfiguration [mode=" + mode + ", numberOfPlayers=" + numberOfPlayers + ", idMap=" + idMap + "]";
+	}
+
+	public void setIdMap(String idMap) {
+		this.idMap = idMap;
 	}
 
 	public GameMode getMode() {
 		return mode;
 	}
+
+	public int getNumberOfPlayers() {
+		return numberOfPlayers;
+	}
+
+	public String getIdMap() {
+		return idMap;
+	}
+
+	private enum GameMode {
+        Easy,
+        Medium,
+        Hard
+    }
 }
